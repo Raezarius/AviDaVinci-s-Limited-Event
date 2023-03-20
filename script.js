3a6aafd38f37f18dda57615e4fa054e4c3786932
Raezarius <raezlan@gmail.com>
2023-03-20 21:15:28 +0800

    More font testing

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var qwindow = document.querySelector('#qwImage');
-var camera = document.querySelector('#camera');
+// var qwindow = document.querySelector('#qwImage');
+// var camera = document.querySelector('#camera');
 
-const loadTime = setInterval(function() {
-    var cameraPos = camera.getAttribute('position');
-    var cameraRot = camera.getAttribute('rotation');
-    var xn = cameraPos.x;                               
-    var yn = cameraPos.y;
-    qwindow.setAttribute('position', { x: xn, y: yn, z: qwindow.getAttribute('position').z });
-    qwindow.setAttribute('rotation', cameraRot);
-}, 10);
+// const loadTime = setInterval(function() {
+//     var cameraPos = camera.getAttribute('position');
+//     var cameraRot = camera.getAttribute('rotation');
+//     var xn = cameraPos.x;                               
+//     var yn = cameraPos.y;
+//     qwindow.setAttribute('position', { x: xn, y: yn, z: qwindow.getAttribute('position').z });
+//     qwindow.setAttribute('rotation', cameraRot);
+// }, 10);
