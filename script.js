9b37465f070f0011f199a0f5700381124bda1f8e
Raezarius <raezlan@gmail.com>
2023-03-21 17:44:12 +0800

    Button Logic testing

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-// var qwindow = document.querySelector('#qwImage');
-// var camera = document.querySelector('#camera');
+function acceptQuest(){
+    alert("ACCEPTED");
+}
 
-// const loadTime = setInterval(function() {
-//     var cameraPos = camera.getAttribute('position');
-//     var cameraRot = camera.getAttribute('rotation');
-//     var xn = cameraPos.x;                               
-//     var yn = cameraPos.y;
-//     qwindow.setAttribute('position', { x: xn, y: yn, z: qwindow.getAttribute('position').z });
-//     qwindow.setAttribute('rotation', cameraRot);
-// }, 10);
+function declineQuest(){
+    alert("DECLINED");
+}
