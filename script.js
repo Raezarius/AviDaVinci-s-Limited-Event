41178ae5003f503e2c181edfa09d4f80f61a20e7
Raezarius <raezlan@gmail.com>
2023-03-22 22:05:23 +0800

    further testing

--- script.js
+++ script.js
@@ -41,9 +41,9 @@ function compRemoval(){
 	let a = document.querySelector("#accept");
 	let d = document.querySelector("#decline");
 
-	var acceptFunc = a.components["accept"].onMEnterA();
+	var acceptFunc = a.components["accept"].aEL;
 	a.removeEventListener("mouseenter", acceptFunc);
-	var declineFunc = d.components["decline"].onMEnterD();
+	var declineFunc = d.components["decline"].dEL;
 	d.removeEventListener("mouseenter", declineFunc);
 
 	a.removeAttribute("accept");
