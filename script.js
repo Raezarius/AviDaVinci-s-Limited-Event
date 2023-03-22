631aa9bc8849cd20b41efe0012d3defb020482fd
Raezarius <raezlan@gmail.com>
2023-03-22 16:05:27 +0800

    Testing sfx and link

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 function acceptQuest(){
-    alert("ACCEPTED");
+    let x = document.getElementById("qAccept");
+			x.currentTime = 0;
+			x.play();
 }
 
 function declineQuest(){
-    alert("DECLINED");
+    let x = document.getElementById("qDecline");
+			x.currentTime = 0;
+			x.play();
+}
+
+function mOverSFX(){
+    let x = document.getElementById("mOver");
+			x.currentTime = 0;
+			x.play();
+}
+
+function buttAccept(){
+	let x = document.getElementById("buttClick");
+			x.currentTime = 0;
+			x.play();
+}
+
+function congrats(){
+	let x = document.getElementById("qComplete");
+			x.currentTime = 0;
+			x.play();
+}
+
+function openForms(){
+	window.location.href = "https://www.youtube.com";
+}
+
+const volSet = function setVol(){
+	document.getElementById("mOver").volume = 0.45;
 }
