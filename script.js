9aaca26ed049670a1ae9c8bd7833fdd64286f3cf
Raezarius <raezlan@gmail.com>
2023-04-09 09:34:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function acceptQuest(){
 			var w = window.open('', '_blank');
 			w.focus();
 			w.blur();
-			// window.focus();
+			window.focus();
 			x.onended = function(){
 				openForms(w);
 			};
